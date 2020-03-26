a8bc76a4a8ed8f78e867f054ea12abe6936f5255
Dikshakeshri <dikshakeshri12@gmail.com>
2020-03-26 15:52:51 +0530

    Greeting

---
+++ Greeting/src/Greet.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Greet {
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+System.out.println("hello Enter something");
+String s=sc.next();
+System.out.println(s);
+}
+}
