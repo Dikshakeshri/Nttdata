c31ee7d0963fb5c57d1913368f32e98222f4ff78
Dikshakeshri <62684806+Dikshakeshri@users.noreply.github.com>
2020-03-26 15:54:48 +0530

    Update Greet.java

--- Greeting/src/Greet.java
+++ Greeting/src/Greet.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Greet {
 public static void main(String[] args) {
 	Scanner sc=new Scanner(System.in);
-System.out.println("hello Enter something");
+System.out.println("hello Enter something To Display");
 String s=sc.next();
 System.out.println(s);
 }
